5d1dc99568969fa2c2daf02b8276578216ea32dc
Andre Veigas <l.andreveigas@gmail.com>
2024-04-04 23:08:33 -0300

    Adicionado botões e estilo para calculadora

---
+++ calculadora/src/components/Button.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import './button.css'
+
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default props =>
+    <button className="button">{ props.label }</button>
