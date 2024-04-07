c204d385b29049f1d2705914e68364dbb821af56
Andre Veigas <l.andreveigas@gmail.com>
2024-04-06 22:12:21 -0300

    eventos capturando funcionalidades e digitos de cada botão

--- calculadora/src/components/Button.jsx
+++ calculadora/src/components/Button.jsx
@@ -3,5 +3,19 @@ import './button.css'
 
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default props =>
-    <button className="button">{ props.label }</button>
+export default props =>{
+    let classes = 'button '
+    classes += props.operation ? 'operation' : ''
+    classes += props.double ? 'double' : ''
+    classes += props.triple ? 'triple' : ''
+
+    return(
+        <button
+            onClick={e => props.click && props.click(props.label)}
+            className={classes}>
+             {props.label}
+        </button>
+
+    )
+}
+    
